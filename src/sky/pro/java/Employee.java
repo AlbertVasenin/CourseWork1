8f94e684d75dabbfa180fd3916d5de562868fb46
Albert <albert_vasenin@mail.ru>
2022-11-03 00:31:52 +0400

    Final level

--- src/sky/pro/java/Employee.java
+++ src/sky/pro/java/Employee.java
@@ -4,17 +4,18 @@ import java.util.Objects;
 
 public class Employee {
 
-  private String fullName;
+  private final String fullName;
   private int department;
   private double salary;
-  private int id;
-  private static int counter = 0;
+  private final int id;
+  private static int counter;
 
   public Employee(String fullName, int department, double salary) {
-    id = ++counter;
     this.fullName = fullName;
     this.department = department;
     this.salary = salary;
+    counter++;
+    id = counter;
   }
 
   public String getFIO() {
@@ -41,10 +42,32 @@ public class Employee {
     this.salary = salary;
   }
 
+  public String toStringWithoutDepartment() {
+    return String.format("Работник №%d: %s, зарплата: %.2f рублей", id, fullName, salary);
+  }
+
   @Override
   public String toString() {
-    return String.format("Работник №%d: %s, отдел: %d, зарплата: %.2f рублей", id, fullName,
+    return String.format("Работник №%d: %s, отдел: %d, зарплата: %.2f рублей\n", id, fullName,
         department, salary);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Employee employee = (Employee) o;
+    return department == employee.department && Double.compare(employee.salary, salary) == 0
+        && id == employee.id && Objects.equals(fullName, employee.fullName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(fullName, department, salary, id);
   }
 }
 
--- src/sky/pro/java/Main.java
+++ src/sky/pro/java/Main.java
@@ -2,246 +2,89 @@ package sky.pro.java;
 
 public class Main {
 
-  Employee[] storage = new Employee[10];
-
   public static void main(String[] args) {
-    Employee[] storage = {
-        new Employee("Иванов Иван Иванович", 1, 81_000.78),
-        new Employee("Петров Сергей Леонидович", 2, 86_050.21),
-        new Employee("Арзуманян Артур Равильевич", 4, 70_000),
-        new Employee("Сидоров Петр Константинович", 3, 93_850.64),
-        new Employee("Козочкина Юлия Сергеевна", 5, 90_250.13),
-        new Employee("Симонов Александ Сергеевич", 3, 75_120.55),
-        new Employee("Марамзина Елена Анатольевна", 2, 92_850.35)};
-    getStorage(storage);
+    EmployeeBook employeeBook = new EmployeeBook(10);
+    employeeBook.addEmployeeBook("Иванов Иван Иванович", 1, 81_000.78);
+    employeeBook.addEmployeeBook("Петров Сергей Леонидович", 2, 86_050.21);
+    employeeBook.addEmployeeBook("Арзуманян Артур Равильевич", 4, 70_000);
+    employeeBook.addEmployeeBook("Сидоров Петр Константинович", 3, 93_850.64);
+    employeeBook.addEmployeeBook("Козочкина Юлия Сергеевна", 5, 90_250.13);
+    employeeBook.addEmployeeBook("Симонов Александр Сергеевич", 3, 75_120.55);
+    employeeBook.addEmployeeBook("Марамзина Елена Анатольевна", 2, 92_850.35);
+    employeeBook.addEmployeeBook("Петров Виктор Владимирович", 1, 65_050.78);
+    employeeBook.addEmployeeBook("Валов Иван Леонидович", 5, 61_406.78);
     separator();
-    System.out.printf("Сумма затрат на зарплаты сотрудников в месяц: %.2f \n",
-        checkSumSalary(storage));
+    System.out.println("Список данных о сотрудниках компании:");
+    employeeBook.getAllEmployee();
     separator();
-    findMinSalary(storage);
-    System.out.print("Сотрудник с минимальной зарплатой:\n");
-    findEmployeeWithMinSalary(storage);
+    System.out.printf("Сумма затрат на зарплаты сотрудников компании в месяц: %.2f рублей\n",
+        employeeBook.getSumSalary());
     separator();
-    findMaxSalary(storage);
-    System.out.print("Сотрудник с максимальной зарплатой:\n");
-    findEmployeeWithMaxSalary(storage);
+    System.out.println(
+        "Минимальная зарплата сотрудника в компании: " + employeeBook.getMinSalary() + " рублей");
+    employeeBook.getEmployeeWithMinSalary();
     separator();
-    System.out.printf("Среднее значение всех зарплат сотрудников: %.2f рублей\n",
-        averageSalaryOfAllEmployees(storage));
+    System.out.println(
+        "Максимальная зарплата сотрудника в компании: " + employeeBook.getMaxSalary() + " рублей");
+    employeeBook.getEmployeeWithMaxSalary();
     separator();
-    checkFullNameEmployees(storage);
+    System.out.printf("Среднее значение всех зарплат сотрудников в компании: %.2f рублей\n",
+        employeeBook.getAverageSalaryOfEmployees());
     separator();
-    //повышенный уровень
-    System.out.print("Зарплата сотрудников после индексации:\n");
-    indexSalary(storage);
+    System.out.println("ФИО сотрудников компании: ");
+    employeeBook.getFullNameEmployees();
     separator();
-    System.out.print("Информация обо всех работниках из одного отдела:\n");
-    filterDepartment(storage, 3);
+    //Повышенный уровень
+    System.out.println("Зарплата сотрудников компании после индексации: ");
+    employeeBook.indexSalary(5);
+    employeeBook.showIndexSalary();
     separator();
-    System.out.print("Минимальная зарплата в выбранном отделе:\n");
-    System.out.printf("%.2f рублей\n", findMinSalaryInDepartment(storage, 3));
+    System.out.println("Минимальная зарплата в выбранном отделе:\n" +
+        employeeBook.getMinSalaryDepartment(1));
     separator();
-    System.out.print("Максимальная зарплата в выбранном отделе:\n");
-    System.out.printf("%.2f рублей\n", findMaxSalaryInDepartment(storage, 3));
+    System.out.println("Максимальная зарплата в выбранном отделе:\n" +
+        employeeBook.getMaxSalaryDepartment(1));
     separator();
-    System.out.print("Сумма затрат на зарплаты сотрудников в отделе:\n");
-    System.out.printf("%.2f рублей\n", checkSumSalaryDepartment(storage, 3));
+    System.out.printf("Сумма затрат на зарплату по отделу: %.2f рублей\n",
+        employeeBook.getSumSalaryDepartment(3));
     separator();
-    System.out.println("Среднее значение всех зарплат сотрудников в отделе:");
-    System.out.printf("%.2f рублей\n", averageSalaryInDepartment(storage, 2));
+    System.out.printf("Среднее значение всех зарплат сотрудников в отделе: %.2f рублей\n",
+        employeeBook.getAverageSalaryDepartment(5));
     separator();
-    System.out.println("Зарплата сотрудников выбранного отдела после индексации:");
-    indexSalaryInDepartment(storage, 3, 5);
+    System.out.println("Зарплата сотрудников выбранного отдела после индексации: ");
+    employeeBook.indexSalaryInDepartment(3, 5);
     separator();
-    System.out.println("Информация о сотрудниках отдела без вывода номера отдела:");
-    printEmployeesDepartment(storage, 2);
+    System.out.println("Данные о работниках отдела: ");
+    employeeBook.showEmployeeDataInDepartment(4);
     separator();
-    System.out.println("Все сотрудники с зарплатой меньше введенного числа:");
-    findLowerSalary(storage, 87_000);
+    System.out.println("Все сотрудники компании с зарплатой меньше введенного числа:");
+    employeeBook.findLowerSalary(90_000);
     separator();
     System.out.println("Все сотрудники с зарплатой больше введенного числа:");
-    findHigherSalary(storage, 70_000);
+    employeeBook.findHigherSalary(85_000);
     separator();
+    //Создаем нового сотрудника, кладем в свободную ячейку массива
+    employeeBook.addEmployeeBook("Галицына Анастасия Вадимовна", 1, 94_235);
+    employeeBook.getAllEmployee();
+    separator();
+    //Удаляем сотрудника по id
+    employeeBook.deleteEmployee(2);
+    //Проверяем в списке удаленного сотрудника
+    employeeBook.getAllEmployee();
+    separator();
+    //Изменяем зарплату сотруднику по ФИО
+    employeeBook.setSalaryEmployees("Валов Иван Леонидович", 84_361);
+    employeeBook.getAllEmployee();//Проверяем
+    separator();
+    //Изменяем отдел сотрудника по ФИО
+    employeeBook.setDepartmentEmployee("Галицына Анастасия Вадимовна", 4);
+    employeeBook.getAllEmployee();
+    separator();
+    employeeBook.printEmployeesDepartment();
   }
 
   public static void separator() {
     System.out.println(
         "==========================================================================");
-  }
-
-  public static void getStorage(Employee[] storage) {
-    for (Employee employee : storage) {
-      if (employee != null) {
-        System.out.println((employee.toString()));
-      }
-    }
-  }
-
-  public static double checkSumSalary(Employee[] storage) {
-    double sum = 0;
-    for (Employee employee : storage) {
-      sum += employee.getSalary();
-    }
-    return sum;
-  }
-
-  public static double findMinSalary(Employee[] storage) {
-    double min = storage[0].getSalary();
-    for (Employee employee : storage) {
-      if (employee.getSalary() < min) {
-        min = employee.getSalary();
-      }
-    }
-    return min;
-  }
-
-  public static void findEmployeeWithMinSalary(Employee[] storage) {
-    for (Employee employee : storage) {
-      if (employee.getSalary() == findMinSalary(storage)) {
-        System.out.println(employee);
-      }
-    }
-  }
-
-  public static double findMaxSalary(Employee[] storage) {
-    double max = storage[0].getSalary();
-    for (Employee employee : storage) {
-      if (employee.getSalary() > max) {
-        max = employee.getSalary();
-      }
-    }
-    return max;
-  }
-
-  public static void findEmployeeWithMaxSalary(Employee[] storage) {
-    for (Employee employee : storage) {
-      if (employee.getSalary() == findMaxSalary(storage)) {
-        System.out.println(employee);
-      }
-    }
-  }
-
-  public static double averageSalaryOfAllEmployees(Employee[] storage) {
-    double average = 0;
-    for (int i = 0; i < storage.length; i++) {
-      average = checkSumSalary(storage) / storage.length;
-    }
-    return average;
-  }
-
-  public static void checkFullNameEmployees(Employee[] storage) {
-    for (Employee employees : storage) {
-      if (employees != null) {
-        System.out.printf("%s \n", employees.getFIO());
-      }
-    }
-  }
-
-  //метод filterDepartment создан для проверки min/max зарплат сотрудников в отделе
-  public static void filterDepartment(Employee[] storage, int department) {
-    int i = 0;
-    while (i < storage.length) {
-      if (storage[i].getDepartment() == department && storage[i] != null) {
-        System.out.println(storage[i].toString());
-      }
-      i++;
-    }
-  }
-
-  public static void indexSalary(Employee[] storage) {
-    int percent = 5;
-    for (Employee employees : storage) {
-      System.out.printf("%s: %.2f рублей\n", employees.getFIO(),
-          (employees.getSalary() + (employees.getSalary() * percent) / 100));
-    }
-  }
-
-  public static double findMinSalaryInDepartment(Employee[] storage, int department) {
-    double min = Double.MAX_VALUE;
-    for (Employee employee : storage) {
-      if (employee != null && employee.getDepartment() == department
-          && employee.getSalary() < min) {
-        min = employee.getSalary();
-      }
-    }
-    return min;
-  }
-
-
-  public static double findMaxSalaryInDepartment(Employee[] storage, int department) {
-    double max = Double.MIN_VALUE;
-    int i = 0;
-    while (i < storage.length) {
-      if (storage[i] != null && storage[i].getDepartment() == department
-          && storage[i].getSalary() > max) {
-        max = storage[i].getSalary();
-      }
-      i++;
-    }
-    return max;
-  }
-
-  public static double checkSumSalaryDepartment(Employee[] storage, int department) {
-    double sum = 0;
-    int i = 0;
-    while (i < storage.length) {
-      if (storage[i] != null && storage[i].getDepartment() == department) {
-        sum += storage[i].getSalary();
-      }
-      i++;
-    }
-    return sum;
-  }
-
-  public static double averageSalaryInDepartment(Employee[] storage, int department) {
-    double average = 0;
-    int count = 0;
-    for (Employee employee : storage) {
-      if (employee != null && employee.getDepartment() == department) {
-        count++;
-        average += employee.getSalary();
-      }
-    }
-    return average / count;
-  }
-
-  public static void indexSalaryInDepartment(Employee[] storage, int department, int percent) {
-    double salary = 0;
-    for (Employee employee : storage) {
-      if (employee != null && employee.getDepartment() == department) {
-        salary = employee.getSalary() + (employee.getSalary() * percent) / 100;
-        System.out.printf("%s: %.2f рублей\n", employee.getFIO(), salary);
-      }
-    }
-  }
-
-  public static void printEmployeesDepartment(Employee[] storage, int department) {
-    for (Employee employees : storage) {
-      if (employees != null && employees.getDepartment() == department) {
-        System.out.printf("Работник №%d: %s, зарплата: %.2f\n", employees.getId(),
-            employees.getFIO(),
-            employees.getSalary());
-      }
-    }
-  }
-
-  public static void findLowerSalary(Employee[] storage, double number) {
-    for (Employee employees : storage) {
-      if (employees != null && employees.getSalary() < number) {
-        System.out.printf("Работник №%d: %s, зарплата: %.2f рублей\n", employees.getId(),
-            employees.getFIO(),
-            employees.getSalary());
-      }
-    }
-  }
-
-  public static void findHigherSalary(Employee[] storage, double number) {
-    for (Employee employees : storage) {
-      if (employees != null && employees.getSalary() >= number) {
-        System.out.printf("Работник №%d: %s, зарплата: %.2f рублей\n", employees.getId(),
-            employees.getFIO(),
-            employees.getSalary());
-      }
-    }
   }
 }
